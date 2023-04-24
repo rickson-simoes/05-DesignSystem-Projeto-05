d9a8a6aa05558c7024a63a95d765530b3dbdb46c
Rickson <rickson.simoes@hotmail.com>
2023-04-24 13:01:19 -0300

    :hammer: :packages: StoryBook - VITEJS - Configuration

---
+++ packages/docs/.storybook/main.ts
@@ -0,0 +1,27 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  async viteFinal(config, options) {
+    // Add your configuration here
+    return config;
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  core: {
+    builder: '@storybook/builder-vite'
+  }
+};
+export default config;
