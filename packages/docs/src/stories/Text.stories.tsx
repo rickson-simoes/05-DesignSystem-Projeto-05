026f7dea39c767f83904ee50f3cb5aa668799525
Rickson <rickson.simoes@hotmail.com>
2023-05-05 18:54:47 -0300

    :hammer: Text StoryBook

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam amet adipisci maxime iusto quibusdam. Sit praesentium illum tempora, placeat mollitia quisquam soluta quam facere, rem consequuntur beatae laboriosam pariatur magnam.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
