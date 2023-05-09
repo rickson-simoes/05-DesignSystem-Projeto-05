d0f0384cfc24f1e56dfebe8bec142c7bbbc501ba
Rickson <rickson.simoes@hotmail.com>
2023-05-09 17:12:56 -0300

    :hammer: StoryBook: AVATAR - Storie

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/rickson-simoes.png',
+    alt: 'Rickson Simões',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
