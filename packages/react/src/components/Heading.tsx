f6e0c549b2b4979007118126a76dbbd52b90552c
Rickson <rickson.simoes@hotmail.com>
2023-05-19 16:13:47 -0300

    :wrench: Correcao de tamanho 7xl -> 6xl

--- packages/react/src/components/Heading.tsx
+++ packages/react/src/components/Heading.tsx
@@ -16,7 +16,7 @@ export const Heading = styled('h2', {
       '3xl': { fontSize: '$6xl' },
       '4xl': { fontSize: '$7xl' },
       '5xl': { fontSize: '$8xl' },
-      '7xl': { fontSize: '$9xl' },
+      '6xl': { fontSize: '$9xl' },
     },
   },
 
